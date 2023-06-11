9fc11fc5cf2a8a31c6c6ad2a9cec9a25628f8ef8
Juan Antonio Breña Moral <bren@juanantonio.info>
2023-06-11 07:52:52 +0000

    decoupling js from html

--- src/main/java/info/jab/ms/MainApplication.java
+++ src/main/java/info/jab/ms/MainApplication.java
@@ -16,7 +16,7 @@ public class MainApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
-		readManifest();
+		//readManifest();
 	}
 
 	private static void readManifest() {
