890a15248e65bf8a704472d0ff8d4cf5c52a57fb
Juan Antonio Breña Moral <bren@juanantonio.info>
2023-06-24 20:19:50 +0200

    fixign checkstyle

--- examples/hello-world-servlet/src/test/java/info/jab/ms/MainApplicationE2eTests.java
+++ examples/hello-world-servlet/src/test/java/info/jab/ms/MainApplicationE2eTests.java
@@ -1,5 +1,7 @@
 package info.jab.ms;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,8 +10,6 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
