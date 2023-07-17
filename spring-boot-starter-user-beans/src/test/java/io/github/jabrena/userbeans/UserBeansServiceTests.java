f00a8af42cfea784ba220d8f9c81df3e3fb1f562
Juan Antonio Breña Moral <bren@juanantonio.info>
2023-07-17 17:20:17 +0200

    Hiding nodes for current implementation (#149)

--- spring-boot-starter-user-beans/src/test/java/io/github/jabrena/userbeans/UserBeansServiceTests.java
+++ spring-boot-starter-user-beans/src/test/java/io/github/jabrena/userbeans/UserBeansServiceTests.java
@@ -6,7 +6,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.github.jabrena.support.SupportController;
 import io.github.jabrena.support.TestApplication;
 import java.util.Comparator;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -66,34 +65,5 @@ class UserBeansServiceTests {
 
         //Then
         assertThat(beanListFiltered).hasSize(2);
-    }
-
-    @Disabled
-    @Test
-    void shouldRetrieveBeanDependencyInfo() {
-        //Given
-        //When
-        var list = userBeansService
-            .getBeansDocuments()
-            .stream()
-            .filter(bd -> bd.dependencies().size() > 0)
-            .flatMap(bd -> bd.dependencies().stream())
-            .distinct()
-            .sorted()
-            .map(str -> {
-                try {
-                    Class<?> myClass = this.getClass().getClassLoader().loadClass(str);
-                    System.out.println(myClass.getSimpleName());
-                    return myClass.getSimpleName();
-                } catch (ClassNotFoundException e) {
-                    //Empty on purpose
-                }
-                return "NOT-FOUND-" + str;
-            })
-            //.peek(System.out::println)
-            .toList();
-
-        //Then
-        assertThat(list).hasSizeGreaterThan(0);
     }
 }
