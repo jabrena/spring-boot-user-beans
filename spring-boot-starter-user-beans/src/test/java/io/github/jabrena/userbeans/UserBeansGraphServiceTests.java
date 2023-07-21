eac0fc96526b096a934848878045b4abe773cdfd
Juan Antonio Breña Moral <bren@juanantonio.info>
2023-07-21 19:02:13 +0200

    adding new tests (#163)

--- spring-boot-starter-user-beans/src/test/java/io/github/jabrena/userbeans/UserBeansGraphServiceTests.java
+++ spring-boot-starter-user-beans/src/test/java/io/github/jabrena/userbeans/UserBeansGraphServiceTests.java
@@ -1,10 +1,16 @@
 package io.github.jabrena.userbeans;
 
+import static io.github.jabrena.userbeans.UserBeansGraphService.UNKNOWN_DEPENDENCY;
+import static io.github.jabrena.userbeans.UserBeansGraphService.UNKNOWN_PACKAGE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.github.jabrena.support.SupportController;
 import io.github.jabrena.support.TestApplication;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.DisplayName;
@@ -161,6 +167,7 @@ class UserBeansGraphServiceTests {
         //When
         var result = userBeansGraphService.getDependencyDocuments().stream()
                 .filter(dd -> dd.dependency().contains(dependency))
+                .peek(System.out::println)
                 .toList();
 
         //Then
@@ -168,5 +175,60 @@ class UserBeansGraphServiceTests {
                 .as("Result of dependency documents should not be empty")
                 .hasSizeGreaterThan(0);
         // @formatter:on
+    }
+
+    @Test
+    void getDependencyDocumentsWithFilter3() {
+        //Given
+        var dependency = "jackson-databind-2.15.0.jar";
+
+        // @formatter:off
+
+        //When
+        var result = userBeansGraphService.getDependencyDocuments().stream()
+                .filter(dd -> dd.dependency().contains(dependency))
+                .peek(System.out::println)
+                .toList();
+
+        //Then
+        assertThat(result)
+                .as("Result of dependency documents should not be empty")
+                .hasSizeGreaterThan(0);
+        // @formatter:on
+    }
+
+    @Test
+    void shouldProvideDifferentNodes() {
+        //Given
+        var expectedUnnamedBeanCounter = 0;
+
+        //When
+        AtomicInteger counter = new AtomicInteger(0);
+
+        var dependencyDocuments = userBeansGraphService.getDependencyDocuments();
+        List<UserBeansGraphService.BeanNode> beanNodeList = dependencyDocuments
+            .stream()
+            .map(dd -> new UserBeansGraphService.BeanNode(dd.beanName(), dd.beanPackage(), dd.dependency(), counter.incrementAndGet()))
+            .distinct()
+            .toList();
+        List<String> listDependencies = dependencyDocuments.stream().flatMap(dd -> dd.beanDependencies().stream()).toList();
+        List<UserBeansGraphService.BeanNode> matchingNodes = listDependencies
+            .stream()
+            .map(dep ->
+                beanNodeList
+                    .stream()
+                    .filter(node -> dep.equals(node.beanName()))
+                    .findFirst()
+                    .orElse(new UserBeansGraphService.BeanNode(dep, UNKNOWN_PACKAGE, UNKNOWN_DEPENDENCY, counter.incrementAndGet()))
+            )
+            .toList();
+
+        Set<UserBeansGraphService.BeanNode> distinctBeans = new HashSet<>(beanNodeList);
+        distinctBeans.addAll(matchingNodes);
+
+        distinctBeans.stream().map(UserBeansGraphService.BeanNode::beanName).distinct().sorted().forEach(System.out::println);
+
+        //Then
+        assertThat(distinctBeans).hasSizeGreaterThan(expectedUnnamedBeanCounter);
     }
 }
