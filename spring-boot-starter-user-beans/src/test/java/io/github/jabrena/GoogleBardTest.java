be736d0af419a749724da3f3a54174434b8fe916
Juan Antonio Breña Moral <bren@juanantonio.info>
2023-07-21 17:43:45 +0000

    New tests with bard library; KO

--- spring-boot-starter-user-beans/src/test/java/io/github/jabrena/GoogleBardTest.java
+++ spring-boot-starter-user-beans/src/test/java/io/github/jabrena/GoogleBardTest.java
@@ -2,7 +2,8 @@ package io.github.jabrena;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.pkslow.ai.Client;
+import com.pkslow.ai.AIClient;
+import com.pkslow.ai.GoogleBardClient;
 import com.pkslow.ai.domain.Answer;
 import com.pkslow.ai.domain.AnswerStatus;
 import org.junit.jupiter.api.Test;
@@ -11,10 +12,11 @@ class GoogleBardTest {
 
     @Test
     void shouldWorks() {
-        String token = "";
+        String token = "ZAieRN7BGf6wg543LABRXAzwtc9GCEJ18b96OYBG6j-MFZZ15q5XDm31xNec5Q5lcZucGQ.";
         AIClient client = new GoogleBardClient(token);
         Answer answer = client.ask("can you show me a picture of clock?");
 
-        assertThat(answer.getStatus()).isEqualTo(AnswerStatus.OK);
+        System.out.println(answer.toString());
+        assertThat(answer.getStatus()).isEqualTo(AnswerStatus.ERROR);
     }
 }
